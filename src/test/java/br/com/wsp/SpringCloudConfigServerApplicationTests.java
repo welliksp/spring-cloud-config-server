4a9ec0333c842d0b0d37200449c4e77a850abdf0
Wellik Perroni <welliksp@gmail.com>
2025-07-29 10:32:27 -0300

    feat: Initialize Spring Cloud Config Server project with Maven wrapper, application configurations, and example profiles.

---
+++ src/test/java/br/com/wsp/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
